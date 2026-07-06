097e1957dea828b85de87f7f43c942d9ad9344e1
Viseth <visethsopheach@gmail.com>
2026-07-06 17:29:59 +0700

    change seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -10,29 +10,29 @@ use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        // Create Admin User
-        User::firstOrCreate(
-            ['email' => 'admin@skybot.com'],
-            [
-                'name' => 'Admin User',
-                'password' => Hash::make('password'),
-                'role' => UserRole::ADMIN,
-            ]
-        );
+  /**
+   * Run the database seeds.
+   */
+  public function run(): void
+  {
+    // Create Admin User
+    User::firstOrCreate(
+      ['email' => 'admin@gmail.com'],
+      [
+        'name' => 'Admin User',
+        'password' => Hash::make('password'),
+        'role' => UserRole::ADMIN,
+      ]
+    );
 
-        // Create Regular User
-        User::firstOrCreate(
-            ['email' => 'user@skybot.com'],
-            [
-                'name' => 'Regular User',
-                'password' => Hash::make('password'),
-                'role' => UserRole::USER,
-            ]
-        );
-    }
+    // Create Regular User
+    User::firstOrCreate(
+      ['email' => 'user@gmail.com'],
+      [
+        'name' => 'Regular User',
+        'password' => Hash::make('password'),
+        'role' => UserRole::USER,
+      ]
+    );
+  }
 }
